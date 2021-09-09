af2216f2472e7a664caad895146c62ae4f02b244
Brandon Scott <brandongregoryscott@gmail.com>
2021-09-09 07:57:56 -0400

    Implement custom FileUpload component with 'staging', removal and loading icon

--- src/components/file-upload.tsx
+++ src/components/file-upload.tsx
@@ -1,37 +1,134 @@
 import { BucketName } from "enums/bucket-name";
-import { FilePicker, Pane, Spinner, majorScale } from "evergreen-ui";
+import {
+    FilePicker,
+    Pane,
+    Spinner,
+    majorScale,
+    IconButton,
+    UploadIcon,
+    DeleteIcon,
+} from "evergreen-ui";
+import { Fragment, useState } from "react";
 import { useUploadFile } from "utils/hooks/use-upload-file";
+import * as uuid from "uuid";
 
 interface FileUploadProps {
     bucketName: BucketName;
+}
+
+interface FilePickerState {
+    id: string;
+    fileList?: FileList;
 }
 
 const height = majorScale(3);
 
 const FileUpload: React.FC<FileUploadProps> = (props: FileUploadProps) => {
     const { bucketName } = props;
-    const { mutate, isLoading } = useUploadFile(bucketName);
+    const { mutate: uploadFiles, isLoading } = useUploadFile(bucketName);
+    const [filePickers, setFilePickers] = useState([newFilePickerState()]);
+    const updateById = (id: string) => (fileList: FileList) =>
+        setFilePickers((prev: FilePickerState[]) => {
+            const index = prev.findIndex((filePicker) => filePicker.id === id);
+            if (index < 0) {
+                return prev;
+            }
 
-    const handleFileChange = (files: FileList) => {
-        mutate(files[0]);
+            prev[index] = { id, fileList };
+
+            return [...prev];
+        });
+
+    const removeById = (id: string) =>
+        setFilePickers((prev: FilePickerState[]) => {
+            const index = prev.findIndex((filePicker) => filePicker.id === id);
+            if (index < 0) {
+                return prev;
+            }
+
+            const updated = prev.filter((filePicker) => filePicker.id !== id);
+            if (updated.length === 0) {
+                return [newFilePickerState()];
+            }
+
+            return updated;
+        });
+
+    const handleFileChange = (id: string) => (files: FileList) =>
+        updateById(id)(files);
+
+    const handleUpload = (id: string) => () => {
+        const files = filePickers.find(
+            (filePicker) => filePicker.id === id
+        )?.fileList;
+
+        if (files == null) {
+            return;
+        }
+
+        uploadFiles(files![0]);
+
+        removeById(id);
     };
+
+    const hasFiles = (id: string) => {
+        const filePicker = filePickers.find(
+            (filePicker) => filePicker.id === id
+        );
+        if (filePicker == null || filePicker.fileList == null) {
+            return false;
+        }
+
+        return filePicker.fileList.length > 0;
+    };
+
+    const handleRemove = (id: string) => () => removeById(id);
+
     return (
-        <Pane alignItems="center" display="flex" flexDirection="row">
-            <FilePicker
-                disabled={isLoading}
-                height={height}
-                multiple={false}
-                onChange={handleFileChange}
-            />
-            {isLoading && (
-                <Spinner
+        <Pane display="flex" flexDirection="column">
+            {filePickers.map((filePicker) => (
+                <Pane
+                    alignItems="center"
                     display="flex"
-                    marginLeft={majorScale(1)}
-                    size={height}
-                />
-            )}
+                    flexDirection="row"
+                    key={filePicker.id}>
+                    <FilePicker
+                        disabled={isLoading}
+                        height={height}
+                        multiple={false}
+                        onChange={handleFileChange(filePicker.id)}
+                    />
+                    {isLoading && (
+                        <Spinner
+                            display="flex"
+                            marginLeft={majorScale(1)}
+                            size={height}
+                        />
+                    )}
+                    {!isLoading && (
+                        <Fragment>
+                            <IconButton
+                                height={height}
+                                icon={DeleteIcon}
+                                onClick={handleRemove(filePicker.id)}
+                            />
+                            {hasFiles(filePicker.id) && (
+                                <IconButton
+                                    height={height}
+                                    icon={UploadIcon}
+                                    onClick={handleUpload(filePicker.id)}
+                                />
+                            )}
+                        </Fragment>
+                    )}
+                </Pane>
+            ))}
         </Pane>
     );
 };
+
+const newFilePickerState = (): FilePickerState => ({
+    id: uuid.v4(),
+});
 
 export { FileUpload };
