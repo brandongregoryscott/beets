7c64b6bd46697a756a32dcc214e640ce60eccd79
Brandon Scott <contact@brandonscott.me>
2022-01-15 11:11:17 -0500

    Clear out selected clipboard state when copied, add toast for attempting to copy with empty selection

--- src/utils/hooks/use-clipboard-state.ts
+++ src/utils/hooks/use-clipboard-state.ts
@@ -1,6 +1,7 @@
 import { toaster } from "evergreen-ui";
 import { List } from "immutable";
 import { useAtom } from "jotai";
+import pluralize from "pluralize";
 import { SetStateAction, useCallback } from "react";
 import { useKey } from "rooks";
 import { ClipboardItem } from "types/clipboard-item";
@@ -18,17 +19,29 @@ interface UseClipboardStateResult {
     setSelectedState: (update: SetStateAction<List<ClipboardItem>>) => void;
 }
 
+const EMPTY_CLIPBOARD_ID = "empty";
+
 const useClipboardState = (): UseClipboardStateResult => {
     const [selectedState, setSelectedState] = useAtom(
         SelectedClipboardStateAtom
     );
-    const [copiedState, setCopiedState] = useAtom(CopiedClipboardStateAtom);
+    const [, setCopiedState] = useAtom(CopiedClipboardStateAtom);
 
-    useKey(["cmd", "c"], (_event) => {
-        toaster.notify(`Clipboard copied ${_event.type}`, {
-            id: JSON.stringify(selectedState),
-        });
-    });
+    const handleCopy = useCallback(() => {
+        if (selectedState.isEmpty()) {
+            toaster.notify("There's nothing selected to copy!", {
+                duration: 1,
+                id: EMPTY_CLIPBOARD_ID,
+            });
+            return;
+        }
+        setCopiedState(selectedState);
+        setSelectedState(List<ClipboardItem>());
+        const count = selectedState.count();
+        toaster.notify(`${count} ${pluralize("item", count)} copied!`);
+    }, [selectedState, setCopiedState, setSelectedState]);
+
+    useKey(["cmd", "c"], handleCopy);
 
     const selectItem = useCallback(
         (item: ClipboardItem) => {
@@ -39,7 +52,7 @@ const useClipboardState = (): UseClipboardStateResult => {
 
     const isSelected = useCallback(
         (value: ClipboardItem) =>
-            selectedState.some((item) => item.equals(value)),
+            selectedState.some((item) => item.id === value.id),
         [selectedState]
     );
 
