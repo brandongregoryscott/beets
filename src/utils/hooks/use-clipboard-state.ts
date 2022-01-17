eef01fca3349e7d7a947b769253492ad421e896e
Brandon Scott <contact@brandonscott.me>
2022-01-17 18:50:32 -0500

    Remove unused import

--- src/utils/hooks/use-clipboard-state.ts
+++ src/utils/hooks/use-clipboard-state.ts
@@ -1,7 +1,7 @@
 import { toaster } from "evergreen-ui";
 import { List } from "immutable";
 import { useAtom } from "jotai";
-import React, { SetStateAction, useCallback } from "react";
+import { SetStateAction, useCallback } from "react";
 import { ClipboardItem } from "types/clipboard-item";
 import { SelectedClipboardStateAtom } from "utils/atoms/clipboard-state-atom";
 import {
