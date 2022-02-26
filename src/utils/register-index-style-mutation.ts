77e42c1b341a01d34ddb8f6aac9d5746d0470781
Brandon Scott <contact@brandonscott.me>
2022-02-26 14:37:54 -0500

    Apply boxShadow while playing

--- src/utils/register-index-style-mutation.ts
+++ src/utils/register-index-style-mutation.ts
@@ -6,10 +6,12 @@ let index: number = -1;
 
 const DATA_INDEX = "data-index";
 
-const applyTranslate = (adjustedIndex: number): HTMLDivElement[] => {
+const applyStyles = (adjustedIndex: number): HTMLDivElement[] => {
     const playingElements = getIndexElements(adjustedIndex);
     playingElements.forEach((element) => {
         element.style.transform = "translateY(-2px)";
+        element.style.boxShadow =
+            "0 0 1px rgb(67 90 111 / 30%), 0 2px 4px -2px rgb(67 90 111 / 47%)";
     });
 
     return playingElements;
@@ -29,7 +31,10 @@ const getAttributeValue = (attributeName: string): number | undefined => {
 
 const getIndexElements = (index?: number): HTMLDivElement[] => {
     const selector =
-        index == null ? `[${DATA_INDEX}]` : `[${DATA_INDEX}='${index}']`;
+        index == null
+            ? // Since '0' is falsy, we need to explicitly include it in the "find all" query
+              `[${DATA_INDEX}],[${DATA_INDEX}='0']`
+            : `[${DATA_INDEX}='${index}']`;
     return document.querySelectorAll(selector) as any as HTMLDivElement[];
 };
 
@@ -37,7 +42,7 @@ const registerIndexStyleMutation = () => {
     Tone.Transport.on("stop", () => {
         index = -1;
         const allElements = getIndexElements();
-        removeTranslate(allElements);
+        removeStyles(allElements);
     });
 
     Tone.Transport.scheduleRepeat((time: number) => {
@@ -54,20 +59,21 @@ const registerIndexStyleMutation = () => {
                 startIndex,
             });
 
-            const playingElements = applyTranslate(adjustedIndex);
+            const playingElements = applyStyles(adjustedIndex);
             const pausedElements = difference(
                 getIndexElements(),
                 playingElements
             ) as HTMLDivElement[];
 
-            removeTranslate(pausedElements);
+            removeStyles(pausedElements);
         }, time);
     }, "8n");
 };
 
-const removeTranslate = (elements: HTMLDivElement[]) => {
+const removeStyles = (elements: HTMLDivElement[]) => {
     elements.forEach((element) => {
         element.style.transform = "";
+        element.style.boxShadow = "";
     });
 };
 
