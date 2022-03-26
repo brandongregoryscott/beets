bca60af40acb8edaae1ba9137c53fe55cd4f2df3
Brandon Scott <contact@brandonscott.me>
2022-03-26 19:15:43 -0400

    Add custom header to HelpDialog in preparation for future documentation pages

--- src/components/sidebar/help-dialog.tsx
+++ src/components/sidebar/help-dialog.tsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { Dialog, DialogProps } from "components/dialog";
 import UsageMarkdown from "docs/usage.md";
 import { useQuery } from "utils/hooks/use-query";
-import { Spinner } from "evergreen-ui";
+import { Spinner, Tablist, Tab, IconButton, CrossIcon } from "evergreen-ui";
 import { Markdown } from "components/markdown";
+import { Flex } from "components/flex";
 
 interface HelpDialogProps extends Pick<DialogProps, "onCloseComplete"> {}
 
+enum HelpTab {
+    Usage = "Usage",
+}
+
+const tabs = [HelpTab.Usage];
+
 const HelpDialog: React.FC<HelpDialogProps> = (props: HelpDialogProps) => {
     const { onCloseComplete } = props;
+    const [selectedTab, setSelectedTab] = useState<HelpTab>(HelpTab.Usage);
     const { resultObject: markdownContent, isLoading } = useQuery<string>({
         fn: async () => {
             const response = await fetch(UsageMarkdown);
@@ -17,9 +25,34 @@ const HelpDialog: React.FC<HelpDialogProps> = (props: HelpDialogProps) => {
         },
     });
 
+    const handleTabSelected = useCallback(
+        (tab: HelpTab) => () => setSelectedTab(tab),
+        []
+    );
+
     return (
         <Dialog
             hasFooter={false}
+            header={({ close }) => (
+                <Flex.Row alignItems="center" width="100%">
+                    <Tablist>
+                        {tabs.map((tab) => (
+                            <Tab
+                                isSelected={tab === selectedTab}
+                                key={tab}
+                                onSelect={handleTabSelected(tab)}>
+                                {tab}
+                            </Tab>
+                        ))}
+                    </Tablist>
+                    <IconButton
+                        appearance="minimal"
+                        icon={CrossIcon}
+                        marginLeft="auto"
+                        onClick={close}
+                    />
+                </Flex.Row>
+            )}
             isShown={true}
             onCloseComplete={onCloseComplete}
             title="Usage"
