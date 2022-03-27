c24bc159029257dd8e197f20543c4e5d246a2961
Brandon Scott <contact@brandonscott.me>
2022-03-27 15:29:33 -0400

    Add failing test case

--- src/utils/collection-utils.test.ts
+++ src/utils/collection-utils.test.ts
@@ -1,4 +1,5 @@
-import { List } from "immutable";
+import { List, Range } from "immutable";
+import { random } from "lodash";
 import { TrackRecord } from "models/track-record";
 import { groupBy } from "./collection-utils";
 
@@ -14,6 +15,41 @@ describe("CollectionUtils", () => {
             const result = groupBy(left, right, matchById);
 
             expect(result).toBeEmpty();
+        });
+
+        it("given object exists in right collection but not left, object is not returned", () => {
+            const left = List<TrackRecord>();
+            const right = List.of(new TrackRecord());
+
+            const result = groupBy(left, right, matchById);
+
+            expect(result).toBeEmpty();
+        });
+
+        it("given object exists in both collections, object is returned", () => {
+            const object = new TrackRecord();
+            const left = List.of(object);
+            const right = List.of(object);
+
+            const result = groupBy(left, right, matchById);
+
+            expect(result).not.toBeEmpty();
+            const grouping = result.first()!;
+            expect(grouping.left).toBe(object);
+            expect(grouping.right).toBe(object);
+        });
+
+        it("given object exists in both collections but is in different order, object is returned", () => {
+            const objects = Range(0, 10)
+                .map(() => new TrackRecord())
+                .toList();
+            const left = List(objects);
+            const right = List(objects).sort(() => random());
+
+            const result = groupBy(left, right, matchById);
+
+            expect(result).not.toBeEmpty();
+            expect(result.count()).toBe(objects.count());
         });
     });
 });
