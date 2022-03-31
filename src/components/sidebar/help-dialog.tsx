9e909485f3683beac73313136c606deae980892e
Brandon Scott <contact@brandonscott.me>
2022-03-31 18:40:35 -0400

    Add button to open help page in new tab at top of dialog

--- src/components/sidebar/help-dialog.tsx
+++ src/components/sidebar/help-dialog.tsx
@@ -1,10 +1,23 @@
 import React, { useCallback, useState } from "react";
 import { Dialog, DialogProps } from "components/dialog";
-import { Spinner, Tablist, Tab, IconButton, CrossIcon } from "evergreen-ui";
+import {
+    Spinner,
+    Tablist,
+    Tab,
+    IconButton,
+    CrossIcon,
+    MinimizeIcon,
+    MaximizeIcon,
+    majorScale,
+    ShareIcon,
+} from "evergreen-ui";
 import { Markdown } from "components/markdown";
 import { Flex } from "components/flex";
 import { HelpResource } from "enums/help-resource";
 import { useHelpDocs } from "utils/hooks/use-help-docs";
+import { useBoolean } from "utils/hooks/use-boolean";
+import { Sitemap } from "sitemap";
+import upath from "upath";
 
 interface HelpDialogProps extends Pick<DialogProps, "onCloseComplete"> {}
 
@@ -15,7 +28,12 @@ const HelpDialog: React.FC<HelpDialogProps> = (props: HelpDialogProps) => {
     const [selectedTab, setSelectedTab] = useState<HelpResource>(
         HelpResource.Usage
     );
+    const { value: isFullscreen, toggle: handleFullscreenClick } = useBoolean();
     const { isLoading, content } = useHelpDocs({ resource: selectedTab });
+
+    const handleShareClick = useCallback(() => {
+        window.open(upath.join(Sitemap.help.home, selectedTab.toLowerCase()));
+    }, [selectedTab]);
 
     const handleTabSelected = useCallback(
         (tab: HelpResource) => () => setSelectedTab(tab),
@@ -24,6 +42,14 @@ const HelpDialog: React.FC<HelpDialogProps> = (props: HelpDialogProps) => {
 
     return (
         <Dialog
+            containerProps={
+                isFullscreen
+                    ? {
+                          marginY: majorScale(2),
+                          maxHeight: `calc(100% - ${majorScale(4)}px)`,
+                      }
+                    : undefined
+            }
             hasFooter={false}
             header={({ close }) => (
                 <Flex.Row alignItems="center" width="100%">
@@ -39,15 +65,28 @@ const HelpDialog: React.FC<HelpDialogProps> = (props: HelpDialogProps) => {
                     </Tablist>
                     <IconButton
                         appearance="minimal"
-                        icon={CrossIcon}
+                        icon={ShareIcon}
                         marginLeft="auto"
+                        onClick={handleShareClick}
+                    />
+                    <IconButton
+                        appearance="minimal"
+                        icon={isFullscreen ? MinimizeIcon : MaximizeIcon}
+                        marginLeft={majorScale(1)}
+                        onClick={handleFullscreenClick}
+                    />
+                    <IconButton
+                        appearance="minimal"
+                        icon={CrossIcon}
+                        marginLeft={majorScale(1)}
                         onClick={close}
                     />
                 </Flex.Row>
             )}
             isShown={true}
             onCloseComplete={onCloseComplete}
-            title="Usage">
+            title="Usage"
+            width={isFullscreen ? "100%" : undefined}>
             {isLoading && <Spinner />}
             {!isLoading && <Markdown>{content}</Markdown>}
         </Dialog>
