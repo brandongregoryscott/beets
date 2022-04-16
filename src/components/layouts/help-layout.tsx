aec634e48e228a8e8b0c9cb592d81325ce996819
Brandon Scott <contact@brandonscott.me>
2022-04-16 14:40:58 -0400

    Add button for scrolling to top of page

--- src/components/layouts/help-layout.tsx
+++ src/components/layouts/help-layout.tsx
@@ -1,9 +1,18 @@
-import { majorScale, Pane, Tab, TabNavigation } from "evergreen-ui";
+import {
+    IconButton,
+    majorScale,
+    Pane,
+    Tab,
+    TabNavigation,
+    CircleArrowUpIcon,
+    Tooltip,
+} from "evergreen-ui";
 import { RouteProps } from "interfaces/route-props";
 import { useNavigate, useLocation, Outlet } from "react-router";
-import { useCallback } from "react";
+import { useCallback, useRef } from "react";
 import { HelpResource } from "enums/help-resource";
 import { joinPaths } from "utils/route-utils";
+import { useTheme } from "utils/hooks/use-theme";
 
 interface HelpLayoutProps extends RouteProps {}
 
@@ -12,6 +21,8 @@ const tabs = [HelpResource.Overview, HelpResource.HowTo];
 const HelpLayout: React.FC<HelpLayoutProps> = (props: HelpLayoutProps) => {
     const location = useLocation();
     const navigate = useNavigate();
+    const { colors } = useTheme();
+    const pageRef = useRef<HTMLDivElement | null>(null);
     const isTabSelected = useCallback(
         (tab: HelpResource): boolean =>
             location.pathname.endsWith(joinPaths(tab)),
@@ -24,8 +35,12 @@ const HelpLayout: React.FC<HelpLayoutProps> = (props: HelpLayoutProps) => {
         [navigate]
     );
 
+    const handleReturnToTopClick = useCallback(() => {
+        pageRef.current?.scrollTo({ top: 0 });
+    }, []);
+
     return (
-        <Pane height="100%" overflow="auto" width="100%">
+        <Pane height="100%" overflow="auto" ref={pageRef} width="100%">
             <Pane marginLeft={majorScale(2)} marginTop={majorScale(2)}>
                 <TabNavigation>
                     {tabs.map((tab) => (
@@ -40,6 +55,19 @@ const HelpLayout: React.FC<HelpLayoutProps> = (props: HelpLayoutProps) => {
                 <Pane margin={majorScale(2)}>
                     <Outlet />
                 </Pane>
+            </Pane>
+            <Pane
+                bottom={majorScale(3)}
+                position="absolute"
+                right={majorScale(4)}>
+                <Tooltip content="Return to top">
+                    <IconButton
+                        appearance="minimal"
+                        background={colors.white}
+                        icon={CircleArrowUpIcon}
+                        onClick={handleReturnToTopClick}
+                    />
+                </Tooltip>
             </Pane>
         </Pane>
     );
