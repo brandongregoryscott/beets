d8bf01b76691fc1159ae09d81c46c6e6bafe6002
Brandon Scott <contact@brandonscott.me>
2022-04-18 13:06:42 -0400

    Add select menu for sorting FileListing, update equality check in wrapped SelectMenu to use isEqual wrapper

--- src/utils/select-menu-utils.ts
+++ src/utils/select-menu-utils.ts
@@ -1,4 +1,8 @@
 import { SelectMenuItem } from "components/select-menu";
+import { SortOrder } from "enums/sort-order";
+import { titleCase } from "humanize-plus";
+import { SortOptions } from "interfaces/sort-options";
+import { flatMap, kebabCase } from "lodash";
 
 const enumToSelectMenuItems = <TValue, TEnum extends Record<string, TValue>>(
     value: TEnum
@@ -9,4 +13,28 @@ const enumToSelectMenuItems = <TValue, TEnum extends Record<string, TValue>>(
         value,
     }));
 
-export { enumToSelectMenuItems };
+const formatSortOptionLabel = <T>(key: keyof T, sortOrder: SortOrder) =>
+    `${titleCase((key as string).replace(/_/, " "))} (${sortOrder})`;
+
+const toSortOptions = <T>(
+    keys: Array<keyof T>
+): Array<SelectMenuItem<SortOptions<T>>> =>
+    flatMap(keys, (key) =>
+        Object.values(SortOrder).map((sortOrder) =>
+            toSortOption(key, sortOrder)
+        )
+    );
+
+const toSortOption = <T>(
+    key: keyof T,
+    sortOrder: SortOrder
+): SelectMenuItem<SortOptions<T>> => ({
+    label: formatSortOptionLabel(key, sortOrder),
+    id: kebabCase(`${key}-${sortOrder}`),
+    value: {
+        column: key,
+        order: sortOrder,
+    },
+});
+
+export { enumToSelectMenuItems, formatSortOptionLabel, toSortOptions };
