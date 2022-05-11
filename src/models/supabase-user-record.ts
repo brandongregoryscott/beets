84275c7ddc40011b0c75011f9e3135b35e1c4182
Brandon Scott <contact@brandonscott.me>
2022-05-11 07:01:47 -0400

    Update SupabaseUserRecord default values

--- src/models/supabase-user-record.ts
+++ src/models/supabase-user-record.ts
@@ -4,26 +4,28 @@ import { SupabaseUser } from "types/supabase-user";
 import { makeDefaultValues } from "utils/core-utils";
 
 const defaultValues = makeDefaultValues<SupabaseUser>({
-    id: "",
-    invited_at: undefined,
-    identities: undefined,
+    action_link: undefined,
     app_metadata: {
         provider: undefined,
     },
-    user_metadata: {},
     aud: "",
     confirmation_sent_at: undefined,
-    recovery_sent_at: undefined,
-    action_link: undefined,
-    email: undefined,
-    phone: undefined,
-    created_at: "",
     confirmed_at: undefined,
+    created_at: "",
+    email: undefined,
+    email_change_sent_at: undefined,
     email_confirmed_at: undefined,
-    phone_confirmed_at: undefined,
+    id: "",
+    identities: undefined,
+    invited_at: undefined,
     last_sign_in_at: undefined,
+    new_email: undefined,
+    phone: undefined,
+    phone_confirmed_at: undefined,
+    recovery_sent_at: undefined,
     role: undefined,
     updated_at: undefined,
+    user_metadata: {},
 });
 
 class SupabaseUserRecord
