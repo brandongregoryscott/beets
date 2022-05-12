fe4ff5ce88fdec27ae9c381cb29f945b6fc72f44
Brandon Scott <contact@brandonscott.me>
2022-05-12 07:55:44 -0400

    Add tests + rewrite diffUpdatedEntities function with documentation

--- src/utils/collection-utils.test.ts
+++ src/utils/collection-utils.test.ts
@@ -2,38 +2,140 @@ import { List, Range } from "immutable";
 import { random } from "lodash";
 import { TrackRecord } from "models/track-record";
 import { TrackSectionRecord } from "models/track-section-record";
-import { groupBy } from "./collection-utils";
+import { generateId } from "utils/id-utils";
+import { diffUpdatedEntities, groupBy } from "./collection-utils";
 
 describe("CollectionUtils", () => {
+    describe("diffUpdatedEntities", () => {
+        it("should return left-side record when record is not persisted", () => {
+            // Arrange
+            const expected = new TrackRecord().merge({
+                created_on: undefined,
+            });
+            const values = List.of(expected);
+            const initialValues = List.of(expected);
+
+            // Act
+            const result = diffUpdatedEntities(values, initialValues);
+
+            // Assert
+            expect(
+                result.find((record) => record.equals(expected))
+            ).not.toBeNil();
+        });
+
+        it("should return left-side POJO when not persisted", () => {
+            // Arrange
+            const expected = new TrackRecord()
+                .merge({
+                    created_on: undefined,
+                })
+                .toPOJO();
+            const values = List.of(expected);
+            const initialValues = List.of(expected);
+
+            // Act
+            const result = diffUpdatedEntities(values, initialValues);
+
+            // Assert
+            expect(
+                result.find((record) => record.id === expected.id)
+            ).not.toBeNil();
+        });
+
+        it("should not return record that is unmodified", () => {
+            // Arrange
+            const unexpected = new TrackRecord().merge({
+                created_on: generateId(),
+            });
+            const values = List.of(unexpected);
+            const initialValues = List.of(unexpected);
+
+            // Act
+            const result = diffUpdatedEntities(values, initialValues);
+
+            // Assert
+            expect(result).toBeEmpty();
+        });
+
+        it("should return record that is modified from initial collection", () => {
+            // Arrange
+            const initialRecord = new TrackRecord().merge({
+                created_on: generateId(),
+                name: "Initial",
+            });
+            const updatedRecord = initialRecord.merge({ name: "Updated" });
+            const values = List.of(initialRecord);
+            const initialValues = List.of(updatedRecord);
+
+            // Act
+            const result = diffUpdatedEntities(values, initialValues);
+
+            // Assert
+            expect(
+                result.find((record) => record.id === initialRecord.id)
+            ).not.toBeNil();
+        });
+
+        it("should return record that is modified from initial collection when order is different", () => {
+            // Arrange
+            const initialRecord = new TrackRecord().merge({
+                created_on: generateId(),
+                name: "Initial",
+            });
+            const unexpected = new TrackRecord();
+            const updatedRecord = initialRecord.merge({ name: "Updated" });
+            const values = List.of(unexpected, initialRecord);
+            const initialValues = List.of(updatedRecord, unexpected);
+
+            // Act
+            const result = diffUpdatedEntities(values, initialValues);
+
+            // Assert
+            expect(
+                result.find((record) => record.id === initialRecord.id)
+            ).not.toBeNil();
+        });
+    });
+
     describe("groupBy", () => {
         const matchById = (left: TrackRecord, right: TrackRecord): boolean =>
             left.id === right.id;
 
         it("given object exists in left collection but not right, object is not returned", () => {
+            // Arrange
             const left = List.of(new TrackRecord());
             const right = List<TrackRecord>();
 
+            // Act
             const result = groupBy(left, right, matchById);
 
+            // Assert
             expect(result).toBeEmpty();
         });
 
         it("given object exists in right collection but not left, object is not returned", () => {
+            // Arrange
             const left = List<TrackRecord>();
             const right = List.of(new TrackRecord());
 
+            // Act
             const result = groupBy(left, right, matchById);
 
+            // Assert
             expect(result).toBeEmpty();
         });
 
         it("given object exists in both collections, object is returned", () => {
+            // Arrange
             const object = new TrackRecord();
             const left = List.of(object);
             const right = List.of(object);
 
+            // Act
             const result = groupBy(left, right, matchById);
 
+            // Assert
             expect(result).not.toBeEmpty();
             const grouping = result.first()!;
             expect(grouping.left).toBe(object);
@@ -41,6 +143,7 @@ describe("CollectionUtils", () => {
         });
 
         it("given object exists in both collections but is in different order, object is returned", () => {
+            // Arrange
             const objects = Range(0, 10)
                 .map(() => new TrackRecord())
                 .toList();
@@ -51,12 +154,14 @@ describe("CollectionUtils", () => {
                 )
             ).sort(() => random(-1, 1, false));
 
+            // Act
             const result = groupBy(
                 left,
                 right,
                 (left, right) => left.id === right.track_id
             );
 
+            // Assert
             expect(result).not.toBeEmpty();
             expect(result.count()).toBe(left.count());
             left.forEach((left) => {
