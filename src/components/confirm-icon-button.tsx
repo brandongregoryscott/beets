42c4e3595059cdc648f69148db3ff36bfc8d540a
Michael Tyson <myty@users.noreply.github.com>
2022-10-09 15:02:16 -0400

    feat(component): Add ConfirmIconButton and update file card delete button (#287)

---
+++ src/components/confirm-icon-button.tsx
@@ -0,0 +1,49 @@
+import { ConfirmationDialog } from "components/confirmation-dialog";
+import { IconButton } from "components/icon-button";
+import { IconButtonProps as EvergreenIconButtonProps } from "evergreen-ui";
+import React, { ForwardedRef, forwardRef, useCallback, useState } from "react";
+
+interface ConfirmIconButtonProps extends EvergreenIconButtonProps {
+    confirmationDescription?: string;
+    confirmationTitle?: string;
+    onClick?: () => void;
+}
+
+const ConfirmIconButton: React.FC<ConfirmIconButtonProps> = forwardRef(
+    (props: ConfirmIconButtonProps, ref: ForwardedRef<HTMLButtonElement>) => {
+        const { confirmationDescription, confirmationTitle, onClick, ...rest } =
+            props;
+
+        const [isConfirming, setIsConfirming] = useState<boolean>(false);
+
+        const handleConfirmation = useCallback(
+            (close: () => void) => {
+                onClick?.();
+                close();
+            },
+            [onClick]
+        );
+
+        return (
+            <React.Fragment>
+                <IconButton
+                    onClick={() => setIsConfirming(true)}
+                    ref={ref}
+                    {...rest}
+                />
+                {isConfirming &&
+                    confirmationDescription != null &&
+                    confirmationTitle != null && (
+                        <ConfirmationDialog
+                            alertDescription={confirmationDescription}
+                            alertTitle={confirmationTitle}
+                            onCloseComplete={() => setIsConfirming(false)}
+                            onConfirm={handleConfirmation}
+                        />
+                    )}
+            </React.Fragment>
+        );
+    }
+);
+
+export { ConfirmIconButton };
