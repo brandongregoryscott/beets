197e01911db6bc5422cd859fa30ffe539be809b4
Mike Koser <mikekoser@gmail.com>
2022-10-11 19:20:08 -0400

    chore(conditional-tooltip.tsx): replaced shouldRender with standard isShown prop
    The shouldRender adds uneccesary customization to the evergreen tooltip, since it has an isShown
    prop that handles conditional rendering.
    
    fix #286

--- src/components/conditional-tooltip.tsx
+++ src/components/conditional-tooltip.tsx
@@ -1,20 +1,11 @@
-import type { TooltipProps as EvergreenTooltipProps } from "evergreen-ui";
+import type { TooltipProps } from "evergreen-ui";
 import { Tooltip as EvergreenTooltip } from "evergreen-ui";
 import type { PropsWithChildren } from "react";
-import { Fragment } from "react";
-
-interface TooltipProps extends EvergreenTooltipProps {
-    shouldRender?: boolean;
-}
 
 const ConditionalTooltip: React.FC<PropsWithChildren<TooltipProps>> = (
     props: PropsWithChildren<TooltipProps>
 ) => {
-    const { shouldRender = true, showDelay = 500, ...tooltipProps } = props;
-
-    if (!shouldRender) {
-        return <Fragment>{props.children}</Fragment>;
-    }
+    const { showDelay = 500, ...tooltipProps } = props;
 
     return (
         <EvergreenTooltip {...tooltipProps} showDelay={showDelay}>
--- src/components/sequencer/sequencer-step.tsx
+++ src/components/sequencer/sequencer-step.tsx
@@ -61,10 +61,12 @@ const SequencerStep: React.FC<SequencerStepProps> = (
         [index, onChange, value]
     );
 
+    const isContentEmpty = !hasSamples && value.isEmpty();
+
     return (
         <ConditionalTooltip
             content="Select one or more samples to drop in"
-            shouldRender={!hasSamples && value.isEmpty()}>
+            isShown={isContentEmpty ? undefined : false}>
             <Card
                 border={true}
                 cursor={hasSamples ? "pointer" : "not-allowed"}
