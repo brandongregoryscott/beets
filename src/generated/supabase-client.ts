f541aba9cb52ed0b8ab960b50f3b66a68ccee1e4
Brandon Scott <contact@brandonscott.me>
2023-01-28 17:01:12 -0500

    fix: add generated supabase-types file to eslintignore

--- src/generated/supabase-client.ts
+++ src/generated/supabase-client.ts
@@ -1,12 +1,11 @@
+import type { File } from "generated/interfaces/file";
+import type { Instrument } from "generated/interfaces/instrument";
+import type { Pgmigration } from "generated/interfaces/pgmigration";
 import type { Project } from "generated/interfaces/project";
 import type { Track } from "generated/interfaces/track";
 import type { TrackSection } from "generated/interfaces/track-section";
 import type { TrackSectionStep } from "generated/interfaces/track-section-step";
 import type { User } from "generated/interfaces/user";
-import type { File } from "generated/interfaces/file";
-import type { TrackSection } from "generated/interfaces/track-section";
-import type { TrackSectionStep } from "generated/interfaces/track-section-step";
-import type { Instrument } from "generated/interfaces/instrument";
 import { Tables } from "generated/enums/tables";
 import { createClient } from "@supabase/supabase-js";
 import { env } from "utils/env";
@@ -18,17 +17,15 @@ const client = createClient(
 
 const SupabaseClient = {
     ...client,
+    fromFiles: () => client.from<File>(Tables.Files),
+    fromInstruments: () => client.from<Instrument>(Tables.Instruments),
+    fromPgmigrations: () => client.from<Pgmigration>(Tables.Pgmigrations),
     fromProjects: () => client.from<Project>(Tables.Projects),
     fromTracks: () => client.from<Track>(Tables.Tracks),
     fromTrackSections: () => client.from<TrackSection>(Tables.TrackSections),
     fromTrackSectionSteps: () =>
         client.from<TrackSectionStep>(Tables.TrackSectionSteps),
     fromUsers: () => client.from<User>(Tables.Users),
-    fromFiles: () => client.from<File>(Tables.Files),
-    fromTrackSections: () => client.from<TrackSection>(Tables.TrackSections),
-    fromTrackSectionSteps: () =>
-        client.from<TrackSectionStep>(Tables.TrackSectionSteps),
-    fromInstruments: () => client.from<Instrument>(Tables.Instruments),
 };
 
 export { SupabaseClient };
