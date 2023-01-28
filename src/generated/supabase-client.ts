2cea29057beece42bb99833a2fa6fde1e50b8b6f
Brandon Scott <contact@brandonscott.me>
2023-01-28 16:53:50 -0500

    build: include DOM in codegen tsconfig, fix postcodegen script and sort supabase client properties

--- src/generated/supabase-client.ts
+++ src/generated/supabase-client.ts
@@ -2,9 +2,9 @@ import type { File } from "generated/interfaces/file";
 import type { Instrument } from "generated/interfaces/instrument";
 import type { Pgmigration } from "generated/interfaces/pgmigration";
 import type { Project } from "generated/interfaces/project";
-import type { TrackSectionStep } from "generated/interfaces/track-section-step";
-import type { TrackSection } from "generated/interfaces/track-section";
 import type { Track } from "generated/interfaces/track";
+import type { TrackSection } from "generated/interfaces/track-section";
+import type { TrackSectionStep } from "generated/interfaces/track-section-step";
 import type { User } from "generated/interfaces/user";
 import { Tables } from "generated/enums/tables";
 import { createClient } from "@supabase/supabase-js";
@@ -21,10 +21,10 @@ const SupabaseClient = {
     fromInstruments: () => client.from<Instrument>(Tables.Instruments),
     fromPgmigrations: () => client.from<Pgmigration>(Tables.Pgmigrations),
     fromProjects: () => client.from<Project>(Tables.Projects),
+    fromTracks: () => client.from<Track>(Tables.Tracks),
+    fromTrackSections: () => client.from<TrackSection>(Tables.TrackSections),
     fromTrackSectionSteps: () =>
         client.from<TrackSectionStep>(Tables.TrackSectionSteps),
-    fromTrackSections: () => client.from<TrackSection>(Tables.TrackSections),
-    fromTracks: () => client.from<Track>(Tables.Tracks),
     fromUsers: () => client.from<User>(Tables.Users),
 };
 
